de525a2b150eacff212cce062e8b81899bd932bd
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-03-28 14:11:01 -0400

    Updates

---
+++ precache-manifest.cbfee6b4fe710e2f8c05f395cbf38126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9edcefa9993d02254f7d6bdb545b3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6909ad37f752dec9d21b",
+    "url": "/static/css/main.851f1ec8.chunk.css"
+  },
+  {
+    "revision": "6a4b57f705d7bd2c51aa",
+    "url": "/static/js/2.f367ab7c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f367ab7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6909ad37f752dec9d21b",
+    "url": "/static/js/main.d2721217.chunk.js"
+  },
+  {
+    "revision": "c2153b2679224fa4f3ab",
+    "url": "/static/js/runtime-main.437f419c.js"
+  }
+]);
